aaa2e603643402071e8d249e35ce697b36db42a4
dexterchan <dexterchan@gmail.com>
2023-02-15 23:32:20 +0800

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import os
 """The setup script."""
 
 from setuptools import setup, find_packages
@@ -10,7 +10,15 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements_file_lst = ["requirements.txt"]
+install_req = []
+directory = os.getcwd()
+
+for file in requirements_file_lst:
+    with open(os.path.join(directory, file), "r", encoding="utf-8") as fr:
+        req_texts = [n.strip() for n in fr.readlines()]
+        req_texts = [t for t in req_texts if len(t) > 0]
+        install_req.extend(req_texts)
 
 test_requirements = ['pytest>=3', ]
 
@@ -29,16 +37,17 @@ setup(
         'Programming Language :: Python :: 3.8',
     ],
     description="Receive trade signal [buy/sell] from pandas dataframe, calculate the MTM for each time interval",
-    install_requires=requirements,
+    install_requires=install_req,
     license="GNU General Public License v3",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='tradesignal_mtm_runner',
     name='tradesignal_mtm_runner',
-    packages=find_packages(include=['tradesignal_mtm_runner', 'tradesignal_mtm_runner.*']),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dexterchan/tradesignal_mtm_runner',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
